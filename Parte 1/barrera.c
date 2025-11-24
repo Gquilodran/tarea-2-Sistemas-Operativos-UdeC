23e5b0dd2db07437f329c7c96b56ce305e81d5b0
Gquilodran <gquilodran2024@udec.cl>
2025-11-24 19:34:32 -0300

    doc: Se agrego una descripcion de lo que hace cada cosa

--- Parte 1/barrera.c
+++ Parte 1/barrera.c
@@ -2,6 +2,10 @@
 #include "barrera.h"
 
 
+/*
+Esta funcion se encarga de inicializar la barrera, asignando valores a las variables
+del struct y de inicializar el cond y el mutex.
+*/
 void barrera_init(barrera_t* b, int N){
 	b->count = 0;
 	b->etapa = 0;
@@ -9,6 +13,14 @@ void barrera_init(barrera_t* b, int N){
 	pthread_mutex_init(&b->mutex, NULL);
 	pthread_cond_init(&b->cond, NULL);
 }
+
+/*
+Esta es la funcion principal de la barrera, wait. Primero se cierra el mutex y se guardan en bariables locales
+el valor de la etapa y el contador aumenta en 1.
+Luego si el valor de contador de la barrera es menor que el numero de hebras representado por N, la hebra se queda en espera (se duerme).
+En caso contrario, a la variable etapa de la barrera se aumenta en 1 y el contador se devuelve a 0, y con broadcast despiertan
+todas las hebras que hay. Tras esto, se desbloquea el mutex. 
+*/
 void barrera_wait(barrera_t* b){
 	pthread_mutex_lock(&b->mutex);
 		int etapa_local = b->etapa;
@@ -26,6 +38,9 @@ void barrera_wait(barrera_t* b){
 		pthread_mutex_unlock(&b->mutex);
 }
 
+/*
+Esta funcion se encarga de destruir tanto el mutex, como la condicion cond. Esto es indispensable.
+*/
 void barrera_destroy(barrera_t* b){
 	pthread_mutex_destroy(&b->mutex);
 	pthread_cond_destroy(&b->cond);
--- Parte 1/barrera.h
+++ Parte 1/barrera.h
@@ -2,14 +2,22 @@
 #define BARRERA_H
 #include <pthread.h>
 
+
+/*
+Estructura basica de la barrera, con las bariables principales. Estas son a las que todas las hebras
+(que esten ligadas a la misma barrera) podran acceder.
+*/
 typedef struct{
-	int n; //numero total de hebras en espera
+	int n; //numero total de hebras
 	int count; //numero de hebras que han llegado a la barrera
-	int etapa; //etapa donde operan N hebras
+	int etapa; //etapa donde opera una hebra
 	pthread_cond_t cond;  
 	pthread_mutex_t mutex;	
 } barrera_t;
 
+/*
+Aqui declaramos las funciones que tendra barrera. Si esto no se hace, el compilador dara error.
+*/
 void barrera_init(barrera_t* b, int N);
 void barrera_wait(barrera_t* b);
 void barrera_destroy(barrera_t* b);
